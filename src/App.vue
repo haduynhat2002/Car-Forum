dbf2486726a2291a090e68885fb8b7aba1c2840d
dungntth2009038 <73456416+dungntth2009038@users.noreply.github.com>
2022-10-06 10:52:49 +0700

    update template admin

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,48 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+ <div>
+  <div class="wrapper">
+
+<!-- Preloader -->
+<!-- <div class="preloader flex-column justify-content-center align-items-center">
+  <img class="animation__shake" src="/admin/dist/img/AdminLTELogo.png" alt="AdminLTELogo" height="60" width="60">
+</div> -->
+
+<!-- Navbar -->
+<nav-bar></nav-bar>
+<!-- /.navbar -->
+
+<!-- Main Sidebar Container -->
+<aside class="main-sidebar sidebar-dark-primary elevation-4">
+  <!-- Brand Logo -->
+  <a href="index3.html" class="brand-link">
+    <img src="/admin/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+    <span class="brand-text font-weight-light">AdminLTE 3</span>
+  </a>
+
+  <!-- Sidebar -->
+ <side-bar></side-bar>
+  <!-- /.sidebar -->
+</aside>
+
+<!-- Content Wrapper. Contains page content -->
+  <router-view></router-view>
+<!-- /.content-wrapper -->
+
+
+<!-- Control Sidebar -->
+
+<!-- /.control-sidebar -->
+</div>
+<!-- ./wrapper -->
+ </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import NavBar from "./components/NavBar.vue";
+import SideBar from "./components/SideBar.vue";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  components : { NavBar, SideBar }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+
